8b3c8b5cc66ae8b372cd2878d4048b522a0f041c
AthulKrishna <athulkrishna9633141960@gmail.com>
2025-06-03 17:11:44 +0530

    add loader

--- src/pages/User/Outlets/Home.jsx
+++ src/pages/User/Outlets/Home.jsx
@@ -19,7 +19,7 @@ import seekerImg from "/Images/cv.png";
 import businessmanImg from "/Images/businessman-blue.png";
 import AdBannerCarousel from "@/components/User/adBanner";
 import adminAxiosInstance from "@/config/axiosConfig/adminAxiosInstance";
-// import { SuperBallLoader } from "@/components/loader";
+import { SuperBallLoader } from "@/components/loader";
 
 // Animation variants for staggered children
 const containerVariants = {
@@ -109,7 +109,7 @@ export default function Home() {
   if (loading) {
     return (
       <div className="fixed inset-0 z-50 bg-white flex items-center justify-center">
-        <p>Loading...</p>
+        <SuperBallLoader/>
       </div>
     )
   }
