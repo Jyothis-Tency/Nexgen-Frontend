ae267be1a2be5938f675de8b97ab865d631f1265
dhinucphilip-datamate <dhinu.c@datamateindia.com>
2025-06-08 22:59:52 +0530

    added company detail navigation in menu drop dwon employer side

--- src/components/Employer/Header.jsx
+++ src/components/Employer/Header.jsx
@@ -7,6 +7,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { logout } from "@/redux/slices/employer";
 import NavbarEmp from "./NavbarEmp";
 import { useState } from "react";
+import { MdAccountCircle } from "react-icons/md";
+import { VscAccount } from "react-icons/vsc";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -34,7 +36,10 @@ const Header = () => {
   };
 
   const handleHomeNavigation = () => {
-    navigate("/employer/dashboard");
+    navigate("/");
+  };
+  const handleCompanyDetailNavigation = () => {
+    navigate("/employer/company_details");
   };
 
   return (
@@ -105,44 +110,32 @@ const Header = () => {
             </div>
 
             {/* Mobile Dropdown Menu */}
-            <div className="dropdown-menu relative flex shrink-0 group block sm:hidden">
-              {imageError ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-9 h-9 rounded-full border-2 border-gray-300 cursor-pointer"
-                  viewBox="0 0 512 512"
-                >
-                  <path d="M437.02 74.98C388.668 26.63 324.379 0 256 0S123.332 26.629 74.98 74.98C26.63 123.332 0 187.621 0 256s26.629 132.668 74.98 181.02C123.332 485.37 187.621 512 256 512s132.668-26.629 181.02-74.98C485.37 388.668 512 324.379 512 256s-26.629-132.668-74.98-181.02zM111.105 429.297c8.454-72.735 70.989-128.89 144.895-128.89 38.96 0 75.598 15.179 103.156 42.734 23.281 23.285 37.965 53.687 41.742 86.152C361.641 462.172 311.094 482 256 482s-105.637-19.824-144.895-52.703zM256 269.507c-42.871 0-77.754-34.882-77.754-77.753C178.246 148.879 213.13 114 256 114s77.754 34.879 77.754 77.754c0 42.871-34.883 77.754-77.754 77.754zm170.719 134.427a175.9 175.9 0 0 0-46.352-82.004c-18.437-18.438-40.25-32.27-64.039-40.938 28.598-19.394 47.426-52.16 47.426-89.238C363.754 132.34 315.414 84 256 84s-107.754 48.34-107.754 107.754c0 37.098 18.844 69.875 47.465 89.266-21.887 7.976-42.14 20.308-59.566 36.542-25.235 23.5-42.758 53.465-50.883 86.348C50.852 364.242 30 312.512 30 256 30 131.383 131.383 30 256 30s226 101.383 226 226c0 56.523-20.86 108.266-55.281 147.934z" />
-                </svg>
-              ) : (
-                <img
-                  src="https://readymadeui.com/team-1.webp"
-                  alt="Profile picture"
-                  className="w-9 h-9 rounded-full border-2 border-gray-300 cursor-pointer"
-                  onError={() => setImageError(true)}
-                />
-              )}
-              <div className="flex items-center gap-4 py-1 pl-1 text-gray-800">
+            <div className="dropdown-menu relative flex items-center shrink-0 group block sm:hidden">
+              
+                <MdAccountCircle className="w-9 h-5 gray-300 cursor-pointer" />
+              
+              {/* <div className="flex items-center gap-4 py-1 pl-1 text-gray-800">
                 {employerData?.name &&
                   employerData.name
                     .toLowerCase()
                     .replace(/\b\w/g, (char) => char.toUpperCase())}
-              </div>
+              </div> */}
               <div className="dropdown-content hidden group-hover:block shadow-md p-2 bg-white rounded-md absolute z-40 top-9 right-0 w-56">
                 <div className="w-full">
                   {/* Home option in mobile dropdown */}
                   <button
-                    onClick={handleHomeNavigation}
+                    onClick={handleCompanyDetailNavigation}
                     className="text-sm text-gray-800 cursor-pointer flex items-center p-2 rounded-md hover:bg-gray-100 dropdown-item transition duration-300 ease-in-out w-full text-left"
                   >
-                    <svg
+                    {/* <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className="w-4 h-4 mr-3 fill-current"
                       viewBox="0 0 24 24"
                     >
                       <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-                    </svg>
-                    Dashboard
+                    </svg> */}
+                    <VscAccount className="w-5 h-4 mr-3 gray-300 " />
+                    Profile
                   </button>
                   <hr className="my-2 -mx-2" />
                   <p
